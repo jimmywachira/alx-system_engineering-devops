700e27d7b2570efbee4299b1f0814b2a95979cd6
jimmywachira <jwachira40@gmail.com>
2024-02-27 10:55:51 -0800

    0x06-regular_expressions

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+# Prints all the words that start with h, end with n, and has one to five t's in between.
+# ./1-repetition_token_0.rb
+puts ARGV[0].scan(/hbt{2,5}n/).join
